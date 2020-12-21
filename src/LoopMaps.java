e2beb2b2290aa185327c39f7ac45e1e4d0c98d47
SubSkip <craig.e.zieg@gmail.com>
2020-12-21 10:06:22 -0600

    Converted to for loop and added product function

--- src/LoopMaps.java
+++ src/LoopMaps.java
@@ -3,42 +3,43 @@ import java.util.*;
 
 public class LoopMaps {
     private int sum;
-    private int product;
-    // private int solution;
-    ArrayList<Integer> numbers = new ArrayList<Integer>();
+    private int product = 1; // Need to set product to 1 for multiplication
+
+    List<Integer> numbers = new ArrayList<>();
 
     public void getNumbers() {
         Scanner operators = new Scanner(System.in);
-        ArrayList<Integer> numbers = new ArrayList<Integer>();
         System.out.println("Please enter five numbers.");
-        System.out.println("First Number: ");
-        numbers.add(operators.nextInt());
-        System.out.println("Second Number: ");
-        numbers.add(operators.nextInt());
-        System.out.println("Third Number: ");
-        numbers.add(operators.nextInt());
-        System.out.println("Fourth Number: ");
-        numbers.add(operators.nextInt());
-        System.out.println("Fifth Number: ");
-        numbers.add(operators.nextInt());
+        for (int i = 0; i < 5; i++) {
+            numbers.add(operators.nextInt());
+        }
+        // Changed to for loop
+        // System.out.println("First Number: ");
+        // numbers.add(operators.nextInt());
+        // System.out.println("Second Number: ");
+        // numbers.add(operators.nextInt());
+        // System.out.println("Third Number: ");
+        // numbers.add(operators.nextInt());
+        // System.out.println("Fourth Number: ");
+        // numbers.add(operators.nextInt());
+        // System.out.println("Fifth Number: ");
+        // numbers.add(operators.nextInt());
         sum = numbers.stream().mapToInt(n -> n).sum();
         System.out.println("The sum of your numbers is " + sum);
-        Collections.sort(numbers);
-        int small = numbers.get(0);
-        System.out.println("The smallest number is " + small);
-        int big = numbers.get(4);
-        System.out.println("The largest number is " + big);
-        // System.out.println(numbers.size());
-        // System.out.println(numbers.get(4));
+        for (int i = 0; i < 5; ++i) {
+            product *= numbers.get(i);
+        }
+        System.out.println("The product of your numbers is " + product);
+        // Collections.sort(numbers);
+        // int small = numbers.get(0);
+        System.out.println("The smallest number is " + Collections.min(numbers));
+        // int large = numbers.get(4);
+        System.out.println("The largest number is " + Collections.max(numbers));
+        operators.close();
     }
 
     public static void main(String[] args) throws Exception {
         LoopMaps nums = new LoopMaps();
         nums.getNumbers();
-        // nums.add();
-
-        // System.out.println("Fifth Number: ");
-        // System.out.println("Fifth Number: ");
-        // System.out.println("Fifth Number: ");
     }
 }
